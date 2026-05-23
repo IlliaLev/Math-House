59be8c205710cde5009c4b27747b466e9885340a
IlliaLev <illialevuoi@gmail.com>
2026-05-23 15:29:05 +0300

    Added feature to go back to previous page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+
 function Navbar() {
+    const navigate = useNavigate();
     return (
         <div>
             <div className={`
                 h-12 w-full
                 bg-cl-bg-inner-100
+                flex flex-row items-center
             `}>
-
+                <div className="ml-3 p-2 text-cl-gr-from hover:text-cl-text-900 cursor-pointer transition-colors duration-300" onClick={() => navigate(-1)}>
+                    <ArrowLeft/>
+                </div>
             </div>
             <div className="h-0.5 w-full bg-cl-gr-from"/>
         </div>
